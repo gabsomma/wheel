3d8041e6f0606d99c7d3da8501d53e0ec4bb3a1f
Gabriel Somma <gabriel.somma@flo.com>
2023-05-10 16:59:53 -0400

    Initial Commit

---
+++ wheel/wheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheelApp.swift
+//  wheel
+//
+//  Created by Gabriel Somma on 2023-05-10.
+//
+
+import SwiftUI
+
+@main
+struct wheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
